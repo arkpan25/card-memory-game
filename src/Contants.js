676b7a429f17bf68c2239148721ded85bcb03a7c
Pan <arkpan225@gmail.com>
2018-09-20 20:56:47 -0500

    Store Constants in Contains.js

---
+++ src/Contants.js
@@ -0,0 +1,8 @@
+
+
+export const totNum = 36;
+
+export const cardArray = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-gear",  "fa-cube", 
+                          "fa-leaf",   "fa-bicycle", "fa-bomb","fa-cubes","fa-bank","fa-bug", 
+                          "fa-birthday-cake","fa-imdb","fa-beer","fa-fighter-jet","fa-flag",
+                          "fa-flash","fa-car"];
--- src/containers/Game.js
+++ src/containers/Game.js
@@ -7,11 +7,8 @@ import Restart from "../components/Restart";
 import DifficultyLevel from "../components/DifficultyLevel";
 import Timer from './Timer';
 import "./Game.css"
+import {totNum, cardArray} from "../Contants";
 
-const totNum = 36;
-let cardArray = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-gear",  "fa-cube", 
-				"fa-leaf",   "fa-bicycle", "fa-bomb","fa-cubes","fa-bank","fa-bug", 
-				"fa-birthday-cake","fa-imdb","fa-beer","fa-fighter-jet","fa-flag","fa-flash","fa-car"];
 
 export class Game extends Component {
 
